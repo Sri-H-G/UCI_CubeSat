da638c0fb6cbd0c2ac2647f6342ea510d019b408
nathannguyen <nathadn9@uci.edu>
2024-10-15 02:21:36 -0700

    background image added, plus size and position adjustments to welcome message, still need to adjust postion of background image

--- cubesat/src/app/home/page.tsx
+++ cubesat/src/app/home/page.tsx
@@ -1,20 +1,24 @@
 import Navbar from "../comps/Navbar";
+import "../home/home.css"
+import Image from 'next/image';
 
 export default function Home(){
     return(
         <>
+            <title>CubeSat - Home</title>
             <Navbar/>
-            <h1>Home</h1>
             <div className = "top-fragment">
-                <h1>UCI CUBESAT</h1>
-                <h2>Pioneering Next-Gen Thermal Control Technology</h2>
-                <h2>For Space Exploration</h2>
-                <p>
-                    Our mission is to design, test, and launch a modular nanosattelite
-                    into low-Earth orbit. By working with cutting-edge thermal control
-                    technology with the ASPIN lab at UCI, we aim to set a standard for
-                    future student-led space mission and research at UCI.
-                </p>
+                <div className = "description">
+                    <h1>UCI CUBESAT</h1>
+                    <h2>Pioneering Next-Gen Thermal Control Technology</h2>
+                    <h2>For Space Exploration</h2>
+                    <p>
+                        Our mission is to design, test, and launch a modular nanosattelite
+                        into low-Earth orbit. By working with cutting-edge thermal control
+                        technology with the ASPIN lab at UCI, we aim to set a standard for
+                        future student-led space mission and research at UCI.
+                    </p>
+                </div>
             </div>
         </>
     )
