8db780e9e0da4bffad1c4cc006b108d85a2c15dd
Byren Cheema <bscheema@uci.edu>
2024-10-21 21:25:41 -0700

    Added aboutus pages and linked to dropdown button

---
+++ cubesat/src/app/aboutus/meet-the-team/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../../comps/Navbar";
+
+export default function WhatWeDO(){
+    return(
+        <>
+            <Navbar/>
+            <h1>Meet the Team</h1>
+        </>
+    )
+}
---
+++ cubesat/src/app/aboutus/what-we-do/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../../comps/Navbar";
+
+export default function WhatWeDO(){
+    return(
+        <>
+            <Navbar/>
+            <h1>What We Do</h1>
+        </>
+    )
+}
