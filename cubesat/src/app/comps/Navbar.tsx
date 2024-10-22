92e23b7a6fef56bc49e98a74890d6951b4ecab11
Byren Cheema <bscheema@uci.edu>
2024-10-21 21:24:00 -0700

    Added dropdown to navbar for about us section.

--- cubesat/src/app/comps/Navbar.tsx
+++ cubesat/src/app/comps/Navbar.tsx
@@ -1,32 +1,52 @@
-import "../comps/NavbarStyle.css"
-import Image from 'next/image';
+'use client';
+import "../comps/NavbarStyle.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <div className = "navbar">
-            <div id = "cubesat-logo">
-                <a href = '/home'>
+        <div className="navbar">
+            <div id="cubesat-logo">
+                <a href="/home">
                     <Image
-                        src = "/images/CubeSat_logo.png"
-                        alt = "CubeSat logo"
-                        width = {120}
-                        height = {120}
+                        src="/images/CubeSat_logo.png"
+                        alt="CubeSat logo"
+                        width={120}
+                        height={120}
                     />
                 </a>
             </div>
-            <div className = "btnbox">
-                <a href='/home'>
-                    <button className = "navbtn">Home</button>
+            <div className="btnbox">
+                <a href="/home">
+                    <button className="navbtn">Home</button>
                 </a>
-                <a href='/aboutus'>
-                    <button className = "navbtn">About Us</button>
-                </a>
-                <a href='/contact'>
-                    <button className = "navbtn">Contact</button>
+
+                <div
+                    className="dropdown"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <button className="navbtn">About Us</button>
+                    {isHovered && (
+                        <div className="dropdown-content">
+                            <a href="/aboutus/what-we-do" className="dropdown-item">
+                                What We Do
+                            </a>
+                            <a href="/aboutus/meet-the-team" className="dropdown-item">
+                                Meet the Team
+                            </a>
+                        </div>
+                    )}
+                </div>
+
+                <a href="/contact">
+                    <button className="navbtn">Contact</button>
                 </a>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
