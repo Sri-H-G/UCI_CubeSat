c7474680d3b25340e1a7fa7c01e1dd01143e7df2
Nathan Nguyen <nathnguyen7631@gmail.com>
2024-11-06 20:44:55 -0800

    finished footer and finished home page

--- cubesat/src/app/comps/Footer.tsx
+++ cubesat/src/app/comps/Footer.tsx
@@ -1,26 +1,33 @@
 import "../comps/FooterStyle.css"
-import Head from "next/head";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { GoArrowUpRight } from "react-icons/go";
+// import Head from "next/head";
 // import Image from 'next/image';
 
 const Footer = () => {
     return (
         <>
-            <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-                integrity = "sha384-OPgkDlG8t4fBv2fRVg4qbaR2F2x9E3d4gp1iLp5BmPbmbCf2Jo02Vv7IHxXtjV6J"
-                crossOrigin="anonymous"/>
-            </Head>
             <div>
                 <h1 id = "connected-header">Stay Connected</h1>
                 <div className = "socials-box">
                     <div className = "icons-box">
-                        <i className = "fab fa-instagram"></i>
-                        <i className = "fab fa-linkedin"></i>
-                        <p>@ucicubesat</p>
+                        <div id = "instagram-box">
+                            <a href = "https://www.instagram.com/ucicubesat?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                                <FaInstagram id = "instagram-logo"/>
+                            </a>
+                            <a href = "https://www.instagram.com/ucicubesat?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                                <p>@ucicubesat</p>
+                            </a>
+                        </div>
+                        <div id = "linkedin-box">
+                            <a href = "https://www.linkedin.com/company/uci-cubesat/">
+                                <FaLinkedin id = "linkedin-logo"/>
+                            </a>
+                        </div>
 
                     </div>
                     <div className = "contact-info">
-                        <p>Join Us <i className = "fas fa-arrow-right"></i></p>
+                    <div id = "joinus-box"><p>Join Us</p><GoArrowUpRight id = "joinus-arrow"/></div>
                         <p>Based in Irvine, CA</p>
                         <p>team.ucicubesat@gmail..com</p>
                     </div>
